0b588068bd5d1098c7066f331443132316be593a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-22 15:41:06 +0300

    rendering views

--- measurements/views.py
+++ measurements/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Measurement
+from .forms import MeasurementModelForm
 
 def calculate_distance_view(request):
-    pass
+    obj = get_object_or_404(Measurement, id=1)
+    form = MeasurementModelForm(request.POST or None)
+
+    context = {
+        'distance': obj,
+        'form': form,
+    }
+
+    return render(request, 'measurements/main.html', context)
